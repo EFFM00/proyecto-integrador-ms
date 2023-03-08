637e4ac5c2fba4da3ecc0bbbdcfabecefd3b2bee
EFFM00 <fischietto.elena@gmail.com>
2023-03-07 23:30:42 -0300

    load balancer multiple instances

--- gateway-api-base/gateway-api/src/main/java/com/digitalmedia/gateway/GatewayApiApplication.java
+++ gateway-api-base/gateway-api/src/main/java/com/digitalmedia/gateway/GatewayApiApplication.java
@@ -2,8 +2,12 @@ package com.digitalmedia.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class GatewayApiApplication {
 
   public static void main(String[] args) {
